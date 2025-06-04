f756ca451a1e8cffab5401fd387e1fba202dfaae
Aditya <adityashirsatrao007@gmail.com>
2025-06-04 21:18:08 +0530

    Create 3201-DistributeCandiesAmongChildrenIi

---
+++ 3201-DistributeCandiesAmongChildrenIi/3201-DistributeCandiesAmongChildrenIi.java
@@ -0,0 +1,11 @@
+// Last updated: 6/4/2025, 9:18:03 PM
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long res = 0;
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i <= 2 * limit)
+                res += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        }
+        return res;
+    }
+}
