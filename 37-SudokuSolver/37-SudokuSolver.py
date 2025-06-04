7fee9b251167945fa8cc8777c0681733120b7624
Aditya <adityashirsatrao007@gmail.com>
2025-06-04 21:23:49 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/37-SudokuSolver.py
@@ -0,0 +1,29 @@
+# Last updated: 6/4/2025, 9:23:44 PM
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        def is_valid(board, row, col, num):
+            for i in range(9):
+                if board[row][i] == num or board[i][col] == num:
+                    return False
+                if board[3*(row//3) + i//3][3*(col//3) + i%3] == num:
+                    return False
+            return True
+
+        def solve(board):
+            for row in range(9):
+                for col in range(9):
+                    if board[row][col] == '.':
+                        for num in '123456789':
+                            if is_valid(board, row, col, num):
+                                board[row][col] = num
+                                if solve(board):
+                                    return True
+                                board[row][col] = '.'
+                        return False
+            return True
+
+        solve(board)
