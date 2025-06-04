6b836cd44aecd42d78ff945c3c5aefab7c3ba118
Aditya <adityashirsatrao007@gmail.com>
2025-06-04 21:21:17 +0530

    Create 1138-GrumpyBookstoreOwner

---
+++ 1138-GrumpyBookstoreOwner/1138-GrumpyBookstoreOwner.py
@@ -0,0 +1,25 @@
+# Last updated: 6/4/2025, 9:21:12 PM
+class Solution(object):
+    def maxSatisfied(self, customers, grumpy, minutes):
+        """
+        :type customers: List[int]
+        :type grumpy: List[int]
+        :type minutes: int
+        :rtype: int
+        """
+        total_satisfied = 0
+        for i in range(len(customers)):
+            if grumpy[i] == 0:
+                total_satisfied += customers[i]
+        
+        additional_satisfied = 0
+        max_additional_satisfied = 0
+        
+        for i in range(len(customers)):
+            if grumpy[i] == 1:
+                additional_satisfied += customers[i]
+            if i >= minutes and grumpy[i - minutes] == 1:
+                additional_satisfied -= customers[i - minutes]
+            max_additional_satisfied = max(max_additional_satisfied, additional_satisfied)
+        
+        return total_satisfied + max_additional_satisfied
